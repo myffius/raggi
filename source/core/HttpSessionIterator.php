8c630edcc6a410b75ac8175449f4c556787e2455
myffius <andryuhab@yandex.ru>
2013-03-14 18:01:50 +0400

    Create HttpSessionIterator.php

---
+++ source/core/HttpSessionIterator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace raggi\base\sessions;
+
+class HttpSessionIterator implements \Iterator
+{
+	/**
+	 * @var
+	 */
+	private $_keys;
+	/**
+	 * @var int
+	 */
+	private $_currentKeyIndex;
+
+	public function __construct()
+	{
+		$this->_keys = array_keys($_SESSION);
+		$this->_currentKeyIndex = 0;
+	}
+	/**
+	 * (PHP 5 &gt;= 5.0.0)<br/>
+	 * Return the current element
+	 * @link http://php.net/manual/en/iterator.current.php
+	 * @return mixed Can return any type.
+	 */
+	public function current()
+	{
+		return $_SESSION[$this->_keys[$this->_currentKeyIndex]];
+	}
+
+	/**
+	 * (PHP 5 &gt;= 5.0.0)<br/>
+	 * Move forward to next element
+	 * @link http://php.net/manual/en/iterator.next.php
+	 * @return void Any returned value is ignored.
+	 */
+	public function next()
+	{
+		$this->_currentKeyIndex++;
+	}
+
+	/**
+	 * (PHP 5 &gt;= 5.0.0)<br/>
+	 * Return the key of the current element
+	 * @link http://php.net/manual/en/iterator.key.php
+	 * @return mixed scalar on success, or null on failure.
+	 */
+	public function key()
+	{
+		return $this->_keys[$this->_currentKeyIndex];
+	}
+
+	/**
+	 * (PHP 5 &gt;= 5.0.0)<br/>
+	 * Checks if current position is valid
+	 * @link http://php.net/manual/en/iterator.valid.php
+	 * @return boolean The return value will be casted to boolean and then evaluated.
+	 * Returns true on success or false on failure.
+	 */
+	public function valid()
+	{
+		return $this->_currentKeyIndex !== false;
+	}
+
+	/**
+	 * (PHP 5 &gt;= 5.0.0)<br/>
+	 * Rewind the Iterator to the first element
+	 * @link http://php.net/manual/en/iterator.rewind.php
+	 * @return void Any returned value is ignored.
+	 */
+	public function rewind()
+	{
+		$this->_currentKeyIndex = 0;
+	}
+}
